a23226b1328c645376193042ebd0dd1ae95cd55d
Prateek Phoenix <leavethemails@gmail.com>
2017-08-15 00:14:08 +0530

    added API key to untracked file

--- app/src/main/java/com/execube/genesis/utils/AppConstants.java
+++ app/src/main/java/com/execube/genesis/utils/AppConstants.java
@@ -1,5 +1,7 @@
 package com.execube.genesis.utils;
 
+import com.execube.genesis.BuildConfig;
+
 /**
  * Created by Prateek Phoenix on 4/24/2016.
  */
@@ -13,7 +15,7 @@ public class AppConstants {
     public static final String BASE_URL = "http://api.themoviedb.org/3/movie/";
     public static final String TOP_RATED = "top_rated";
     public static final String POPULAR = "popular";
-    public static final String API_KEY = "a98debe57ccd9b42fe6b99b9014c80e3";
+    public static final String API_KEY = BuildConfig.TMDB_API_KEY;
     public static final String SORT_POPULARITY = "popularity.desc";
     public static final String SORT_R_RATED = "&certification_country=US&certification=R&sort_by=vote_average.desc&vote_count.gte=250";
     public static final String IMAGE_URL = "http://image.tmdb.org/t/p/";
